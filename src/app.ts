d38e8bcc6d5afbd0b5f90212324d0bfb1bac93ea
juaniarrigoni <juani.arrigoni@gmail.comgit config --global user.name juaniarrigonigit initgit config --global user.email juani.arrigoni@gmail.com>
2024-06-07 17:03:02 -0300

    integracion meta developer en docker

--- src/app.ts
+++ src/app.ts
@@ -97,6 +97,9 @@ const fullSamplesFlow = addKeyword<Provider, Database>([
   });
 
 const main = async () => {
+  if (VERIFY_TOKEN) {
+    return VERIFY_TOKEN;
+  }
   const adapterFlow = createFlow([welcomeFlow, registerFlow, fullSamplesFlow]);
   const adapterProvider = createProvider(Provider, {
     jwtToken: JWT_TOKEN,

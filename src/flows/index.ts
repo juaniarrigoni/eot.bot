486a1c26d15379374607e506010f104708c48345
juaniarrigoni <juani.arrigoni@gmail.comgit config --global user.name juaniarrigonigit initgit config --global user.email juani.arrigoni@gmail.com>
2024-06-08 21:35:24 -0300

    Flows debugeados: Flows eliminados

--- src/flows/index.ts
+++
@@ -1,8 +0,0 @@
-import { createFlow } from "@builderbot/bot";
-import { welcomeFlow } from "./welcome.flow";
-import { flowSeller } from "./seller.flow";
-import { flowSchedule } from "./schedule.flow";
-import { flowConfirm } from "./confirm.flow";
-
-
-export default createFlow([welcomeFlow, flowSeller, flowSchedule, flowConfirm])
